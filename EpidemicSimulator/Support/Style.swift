4ef1af90d877ab717cae33243b0e4bf54e074319
itissep <someonenewish@yandex.ru>
2023-05-08 15:51:43 +0300

    add: main colors

---
+++ EpidemicSimulator/Support/Style.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  EpidemicSimulator
+//
+//  Created by Someone on 08.05.2023.
+//
+
+import UIKit
+
+struct Styles {
+    struct Color {
+        static let black = UIColor(named: "BlackColor")
+        static let mustard = UIColor(named: "MustardColor")
+        static let yellow = UIColor(named: "YellowColor")
+    }
+    
+    static let mainFont = UIFont.systemFont(ofSize: 20, weight: .bold)
+}
